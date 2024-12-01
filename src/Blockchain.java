55077a10ff3abdf90e4b77219a873c97a9860aa5
Charalampos Deligiannakis <xaralamposdelig@gmail.com>
2024-12-01 18:08:08 +0200

    Add name attribute to Blockchain class

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -4,11 +4,31 @@ import java.util.Set;
 
 public class Blockchain {
 
+    private String name;
     private ArrayList<Block> blocks;
     private Set<String> hashes;
 
     Blockchain() {
         this.blocks = new ArrayList<>();
         this.hashes = new HashSet<>();
+    }
+
+    Blockchain(String name) throws BlockchainException {
+        setName(name);
+        this.blocks = new ArrayList<>();
+        this.hashes = new HashSet<>();
+    }
+
+    public void setName(String name) throws BlockchainException {
+        if (name.matches("^[A-Za-zΑ-Ωα-ω0-9]+( ?[A-Za-zΑ-Ωα-ω0-9]*)")) {
+            this.name = name;
+        } else {
+            // Throw an exception with an appropriate message and offending value
+            throw new BlockchainException("Invalid receiver name.", name);
+        }
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
