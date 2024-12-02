02846c30fec39a1bf4874e08e0d150e79a3d65cf
Charalampos Deligiannakis <xaralamposdelig@gmail.com>
2024-12-02 14:21:19 +0200

    Create blockchain object in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws BlockchainException, BlockException, TransactionException {
+        Blockchain b = new Blockchain("my blockchain");
+        System.out.println(b.toString());
+        System.out.println(b.getBlocks().getFirst());
     }
 }
