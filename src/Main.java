04162920e96f4669423172540d05c4fab4ea4b04
Charalampos Deligiannakis <xaralamposdelig@gmail.com>
2024-12-12 14:29:17 +0200

    Create Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 public class Main {
     public static void main(String[] args) throws BlockchainException, BlockException, TransactionException {
         Blockchain b = new Blockchain("my blockchain");
-        System.out.println(b.toString());
-        System.out.println(b.getBlocks().getFirst());
+
+        Transaction transaction1 = new Transaction("Bob", "Alice", BigDecimal.valueOf(32.50));
+        b.createBlock(transaction1, false);
+
+        Transaction transaction2 = new Transaction("Mark", "John", BigDecimal.valueOf(1360));
+        b.createBlock(transaction2);
+
+        System.out.println(b.toString(0, b.getBlocks().size()));
+
     }
 }
